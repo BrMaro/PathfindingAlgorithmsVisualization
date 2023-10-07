0ef917afbf2853af2048a95a6e972dda8f88a842
BrMaro <keremamariari@gmail.com>
2023-10-07 22:22:56 +0300

    fixed bug where you could place end node on top of start node

--- main.py
+++ main.py
@@ -148,10 +148,10 @@ def main(win,width):
                 pos = pygame.mouse.get_pos()
                 row,col = get_clicked_pos(pos,ROWS,width)
                 node = grid[row][col]
-                if not start:
+                if not start and node != end:
                     start = node
                     start.make_start()
-                elif not end:
+                elif not end and node != start:
                     end = node
                     end.make_end()
                 elif node != end and node != start:
